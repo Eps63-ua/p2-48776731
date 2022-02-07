0b61ce9e9cc997230df1e0cd944d5a6c7dee08ff
Eps63-ua <eps63@alu.ua.es>
2022-02-07 12:52:11 +0100

    7-02

--- p1/prac1.cc
+++ p1/prac1.cc
@@ -1,9 +1,12 @@
 /* Programación 2 - Práctica 1
  * DNI:48776731X
  * NOMBRE: Esther Peral Soler
+ * Token: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
+ * seed = 333
  */
 #include <iostream>
 #include <cstdlib> // Para rand(), srand() y atoi()
+#include <cstring> //para str()
 
 using namespace std;
 
@@ -40,15 +43,91 @@ struct Hero{
   int kills[KENEMIES];
 };
 
+typedef char Tcadena[KNAME];
+
 int rollDice(){
   return rand()%KDICE+1;
 }
 
 Hero createHero(){
+    Tcadena name;
+    Hero Hero;
+    int attack_points=0, defense_points=0, hp_points=0; 
+    float attack_percent=0, defense_percent=0;
+    bool sum=false;
+    
+    cout<<"Enter hero name: ";
+    cin>>name; 
+    strcpy(Hero.name, name);
+    
+    //comprobar que el nombre es valido
+    
+    while(!sum || attack_percent==0 || defense_percent==0){
+        cout<<"Enter attack/defense: ";
+        cin>>attack_percent>>defense_percent;
+        
+        if((attack_percent+defense_percent)==100){
+            sum=true;
+        }
+    }
+    
+    attack_points = (attack_percent/100)*KPOINTS;
+    defense_points = (defense_percent/100)*KPOINTS;
+    
+    hp_points = defense_points*2;
+         
+         
 }
 
-Enemy createEnemy(){
-}
+Enemy createEnemy(int rollDice){
+    Enemy Enemy;
+    int result;
+    
+   result = rollDice;
+    
+    if(result<=1){
+        
+        Enemy.name = AXOLOTL;
+        Enemy.features.attack = 40;
+        Enemy.features.defense = 40;
+        Enemy.features.hp = Enemy.features.defense*2;
+        
+    }else if(result<=11){
+        
+        Enemy.name = TROLL;
+        Enemy.features.attack = 60;
+        Enemy.features.defense = 80;
+        Enemy.features.hp = Enemy.features.defense*2;
+        
+    }else if(result<=15){
+        
+        Enemy.name = ORC;
+        Enemy.features.attack = 80;
+        Enemy.features.defense = 120;
+        Enemy.features.hp = Enemy.features.defense*2;
+        
+    }else if(result<=18){
+        
+        Enemy.name = HELLHOUND;
+        Enemy.features.attack = 120;
+        Enemy.features.defense = 100;
+        Enemy.features.hp = Enemy.features.defense*2;
+        
+    }else{
+        
+        Enemy.name = DRAGON;
+        Enemy.features.attack = 160;
+        Enemy.features.defense = 140;
+        Enemy.features.hp = Enemy.features.defense*2;
+    }
+    
+    cout<< Enemy.name << endl
+    << Enemy.features.attack << endl
+    << Enemy.features.defense << endl
+    << Enemy.features.hp << endl;
+    
+
+} 
 
 void fight(Hero &hero,Enemy &enemy){
 }
@@ -74,5 +153,8 @@ int main(int argc,char *argv[]){
     srand(atoi(argv[1])); // Introducimos la semilla para generar números aleatorios
     
     // Aquí vendrá todo tu código del "main"...
+     int dicenumber = rollDice();
+    createEnemy(dicenumber);
+    createHero();
   }
 }
--- p1/practica1.cc
+++ p1/practica1.cc
@@ -2,4 +2,6 @@
 // DNI: 48776731X   
 // Nombre: Esther Peral Soler
 
+/*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*/
+
 
